2e71576d3eed18d5817b24b2570ccce84a7c01dd
macbem <maciej.bembenista@gmail.com>
2018-05-28 01:04:58 +0200

    DB configs - test/dev

--- config/dev.exs
+++ config/dev.exs
@@ -39,8 +39,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :todos, Todos.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "macbem",
+  password: "",
   database: "todos_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :todos, Todos.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "macbem",
+  password: "",
   database: "todos_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
